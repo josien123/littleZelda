da39c09be22c98b9a95bf1f47c79f71089962692
josien123 <josielarue@icloud.com>
2026-01-14 22:35:25 -0500

    Remove unused import statement from NonPlayableCharacter class

--- NonPlayableCharacter.java
+++ NonPlayableCharacter.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class NonPlayableCharacter extends Character {
 
   // variables-----------------------------------------------------------------------------------------------------------
